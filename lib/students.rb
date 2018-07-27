2a6622cacf09636342443b0f88ed4dfc85958726
Dani Schuhman <dani8439@aol.com>
2018-07-27 19:37:49 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -10,11 +10,10 @@ end
 
 def average_student_gpa
   "SELECT AVG(gpa) FROM students"
-
 end
 
 def total_tardies_for_all_students
-
+  "SELECT SUM(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
